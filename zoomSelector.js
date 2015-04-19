cd2febea2c10de3edc7893fd70f764e2fd536cbf
MixMasterT <i@mixter-t.com>
2015-04-19 21:23:01 +0800

    add beginPath() and closePath()

--- zoomSelector.js
+++ zoomSelector.js
@@ -1,22 +1,39 @@
-        
 var c = document.getElementById("c");
 var ctx = c.getContext('2d');
         
 var MAX_ZOOM_OUT = 4;
-var currentRange = MAX_ZOOM_OUT; //initialize to full view of Mandlebrot
+
 var pxCenterX = c.width/2;
 var pxCenterY = c.height/2;
 
+//initialize to full view of Mandlebrot centered on [0,0i]
+var currentRange = MAX_ZOOM_OUT; 
+var rX = 0;
+var iY = 0;
+
 document.getElementById("range").oninput = function() {
     document.getElementById("scale").innerHTML = "Zoom factor = " + document.getElementById("range").value;
+        //first get rid of the old rectangle
+    ctx.clearRect( 0 , 0 , c.width , c.height );
     updateZoombox(pxCenterX,pxCenterY);
+}
+
+document.getElementById("c").onclick = function(e) {
+    // update displayed value for r and i:
+    var newR = (rX - currentRange/2) + ((e.pageX - c.offsetLeft)/c.width)*currentRange;
+    var newi = (iY - currentRange/2) + ((e.pageY - c.offsetTop)/c.height)*currentRange;
+    document.getElementById("zoomCenter").innerHTML = "Zoom center: r = " + parseFloat(newR).toFixed(3) + " , i = " + parseFloat(newi).toFixed(3)  ;
+    
+    // update the center for the Zoom box.
+    pxCenterX = e.pageX - c.offsetLeft;
+    pxCenterY = e.pageY - c.offsetTop;
+    
+    
+    
 }
         
 function updateZoombox(centerX, centerY) {
-            
-    //first get rid of the old rectangle
-    ctx.clearRect(0,0,c.width,c.height);
-    
+              
     var scalFact = document.getElementById("range").value;
 
     var newRange = currentRange * Math.pow(2,-scalFact);
@@ -28,6 +45,10 @@ function updateZoombox(centerX, centerY) {
         console.log(pixRange);
         var pX = centerX - pixRange/2;
         var pY = centerY - pixRange/2;
+    
+    //Draw rectangle
+        ctx.beginPath();
         ctx.rect(pX,pY, pixRange, pixRange);
         ctx.stroke();
+        ctx.closePath();
 }
